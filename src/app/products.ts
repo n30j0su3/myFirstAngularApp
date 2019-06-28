4783e81aba5002b943edcd11821e955aaf780c91
N30j0su3 <jorojas1@gmail.com>
2019-06-28 14:32:30 -0500

    Example angular app from angular docs

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Iphone X',
+    price: 1000,
+    description: 'A large Iphone with one of the best screens'
+  },
+  {
+    name: 'IPhone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Moto Z',
+    price: 599,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
